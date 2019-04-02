6048ebf54f534e08310f48effc3b3f736cb02524
David Ralph <ohlookitsderpy@protonmail.com>
2019-04-02 18:10:13 +0100

    part two

--- index.js
+++ index.js
@@ -13,137 +13,190 @@ module.exports = class DiscordRPC extends Plugin {
       let timestamp = new Date().getTime();
 
       const setActivity = async () => {
-          switch (this.settings.get('timestamp')) {
-              case true: {
-                uRPC.send('SET_ACTIVITY', {
-                    pid: process.pid,
-                    activity: {
-                        details: this.settings.get('lineone', 'Hello!'),
-                        state: this.settings.get('linetwo', 'I am cool.'),
-                        assets: {
-                            large_image: this.settings.get('largeimage', 'null'),
-                            large_text: this.settings.get('largeimagetext', 'null'),
-                            small_image: this.settings.get('smallimage', 'null'),
-                            small_text: this.settings.get('smallimagetext', 'null')
-                        },
-                        timestamps: { start: timestamp },
-                        instance: false
-                    }
-                });
-                break;
-              }
-              case false: {
-                uRPC.send('SET_ACTIVITY', {
-                    pid: process.pid,
-                    activity: {
-                        details: this.settings.get('lineone', 'Hello!'),
-                        state: this.settings.get('linetwo', 'I am cool.'),
-                        assets: {
-                            large_image: this.settings.get('largeimage', 'null'),
-                            large_text: this.settings.get('largeimagetext', 'null'),
-                            small_image: this.settings.get('smallimage', 'null'),
-                            small_text: this.settings.get('smallimagetext', 'null')
-                        },
-                        instance: false
-                    }
-                });
-              }
-          }
+        switch (this.settings.get('timestamp')) {
+            case true: {
+              uRPC.send('SET_ACTIVITY', {
+                  pid: process.pid,
+                  activity: {
+                      details: this.settings.get('lineone', 'Hello!'),
+                      state: this.settings.get('linetwo', 'I am cool.'),
+                      assets: {
+                          large_image: this.settings.get('largeimage', 'null'),
+                          large_text: this.settings.get('largeimagetext', 'null'),
+                          small_image: this.settings.get('smallimage', 'null'),
+                          small_text: this.settings.get('smallimagetext', 'null')
+                      },
+                      timestamps: { start: timestamp },
+                      instance: false
+                  }
+              });
+              break;
+            }
+            case false: {
+              uRPC.send('SET_ACTIVITY', {
+                  pid: process.pid,
+                  activity: {
+                      details: this.settings.get('lineone', 'Hello!'),
+                      state: this.settings.get('linetwo', 'I am cool.'),
+                      assets: {
+                          large_image: this.settings.get('largeimage', 'null'),
+                          large_text: this.settings.get('largeimagetext', 'null'),
+                          small_image: this.settings.get('smallimage', 'null'),
+                          small_text: this.settings.get('smallimagetext', 'null')
+                      },
+                      instance: false
+                  }
+              });
+            }
+        }
     }
 
-    uRPC.on('ready', () => {
-        setActivity();
-        setInterval(setActivity, 15e3);
+    uRPC.on('ready', () => { 
+      setActivity(); 
+      setInterval(setActivity, 15e3);
     });
 
-   if (this.settings.get('commandsEnabled') === true) {
-     powercord
-       .pluginManager
-       .get('pc-commands')
-       .register(
-         'timestamp',
-         'Enable/Disable Discord RPC timestamp.',
-         '{c} [true/false]',
-         (args) => {
-          if (args[0] === 'true') return this.settings.set('timestamp', true);
-          else if (args[0] === 'false') return this.settings.set('timestamp', false);
-          return;
-         }
-      );
-
-     powercord
-       .pluginManager
-       .get('pc-commands')
-       .register(
-         'lineone',
-         'Edit line one of the Discord RPC.',
-         '{c} [data]',
-         (args) => {
-          return this.settings.set('lineone', args.join(' '));
-         }
-      );
-
-    powercord
-      .pluginManager
-      .get('pc-commands')
-      .register(
-        'linetwo',
-        'Edit line two of the Discord RPC.',
-        '{c} [data]',
-        (args) => {
-         return this.settings.set('linetwo', args.join(' '));
-        }
-      );
-
-    powercord
-      .pluginManager
-      .get('pc-commands')
-      .register(
-        'largeimage',
-        'Change the large image of the Discord RPC.',
-        '{c} [data]',
-        (args) => {
-         return this.settings.set('largeimage', args.join(' '));
-        }
-      );
-
-    powercord
-      .pluginManager
-      .get('pc-commands')
-      .register(
-        'largeimagetext',
-        'Change the large image hover text on the Discord RPC.',
-        '{c} [data]',
-        (args) => {
-         return this.settings.set('largeimagetext', args.join(' '));
-        }
-      );
-
-    powercord
-      .pluginManager
-      .get('pc-commands')
-      .register(
-        'smallimage',
-        'Change the small image of the Discord RPC.',
-        '{c} [data]',
-        (args) => {
-         return this.settings.set('smallimage', args.join(' '));
-        }
-      );
-
-    powercord
-      .pluginManager
-      .get('pc-commands')
-      .register(
-        'smallimagetext',
-        'Change the small image hover text on the Discord RPC.',
-        '{c} [data]',
-        (args) => {
-         return this.settings.set('smallimagetext', args.join(' '));
-        }
-      );
-    }
+    if (this.settings.get('commandsEnabled')) this.loadCommands();
   }
+
+    modifyCommands() {
+      switch (this.settings.get('commandsEnabled')) {
+        case true: {
+          this.settings.set('commandsEnabled', true);
+          this.loadCommands();
+          break;
+        }
+        case false: {
+          this.settings.set('commandsEnabled', false);
+          this.unloadCommands();
+        }
+      }
+    }
+
+    loadCommands () {
+      powercord
+      .pluginManager
+      .get('pc-commands')
+      .register(
+        'timestamp',
+        'Enable/Disable Discord RPC timestamp.',
+        '{c} [true/false]',
+        (args) => {
+         if (args[0] === 'true') return this.settings.set('timestamp', true);
+         else if (args[0] === 'false') return this.settings.set('timestamp', false);
+         return;
+        }
+     );
+
+    powercord
+      .pluginManager
+      .get('pc-commands')
+      .register(
+        'lineone',
+        'Edit line one of the Discord RPC.',
+        '{c} [data]',
+        (args) => {
+         return this.settings.set('lineone', args.join(' '));
+        }
+     );
+
+   powercord
+     .pluginManager
+     .get('pc-commands')
+     .register(
+       'linetwo',
+       'Edit line two of the Discord RPC.',
+       '{c} [data]',
+       (args) => {
+        return this.settings.set('linetwo', args.join(' '));
+       }
+     );
+
+   powercord
+     .pluginManager
+     .get('pc-commands')
+     .register(
+       'largeimage',
+       'Change the large image of the Discord RPC.',
+       '{c} [data]',
+       (args) => {
+        return this.settings.set('largeimage', args.join(' '));
+       }
+     );
+
+   powercord
+     .pluginManager
+     .get('pc-commands')
+     .register(
+       'largeimagetext',
+       'Change the large image hover text on the Discord RPC.',
+       '{c} [data]',
+       (args) => {
+        return this.settings.set('largeimagetext', args.join(' '));
+       }
+     );
+
+   powercord
+     .pluginManager
+     .get('pc-commands')
+     .register(
+       'smallimage',
+       'Change the small image of the Discord RPC.',
+       '{c} [data]',
+       (args) => {
+        return this.settings.set('smallimage', args.join(' '));
+       }
+     );
+
+   powercord
+     .pluginManager
+     .get('pc-commands')
+     .register(
+       'smallimagetext',
+       'Change the small image hover text on the Discord RPC.',
+       '{c} [data]',
+       (args) => {
+        return this.settings.set('smallimagetext', args.join(' '));
+       }
+     );
+    }
+
+    unloadCommands() {
+      powercord
+        .pluginManager
+        .get('pc-commands')
+        .unregister('lineone');
+
+      powercord
+        .pluginManager
+        .get('pc-commands')
+        .unregister('linetwo');
+
+      powercord
+        .pluginManager
+        .get('pc-commands')
+        .unregister('largeimage');
+
+      powercord
+        .pluginManager
+        .get('pc-commands')
+        .unregister('largeimagetext');
+
+      powercord
+        .pluginManager
+        .get('pc-commands')
+        .unregister('smallimage');
+
+      powercord
+        .pluginManager
+        .get('pc-commands')
+        .unregister('smallimagetext');
+
+      powercord
+        .pluginManager
+        .get('pc-commands')
+        .unregister('timestamp'); 
+    }
 
   pluginWillUnload() {
     remote.getCurrentWindow().reload();
